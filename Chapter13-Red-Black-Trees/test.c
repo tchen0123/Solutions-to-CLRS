b690546d8763fb6585707caf2a3dac2028d9c7d5
leohotfn <leohotfn@qq.com>
2016-04-21 22:41:44 +0800

    Update

---
+++ Chapter13-Red-Black-Trees/test.c
@@ -0,0 +1,54 @@
+/*
+ * Solutions-to-CLRS/Chapter13-Red-Black-Trees/test.c
+ *
+ * Copyright (c) 2016  Jiawei Feng
+ */
+
+#include "rbtree.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static inline void Swap(int *a, int *b)
+{
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+}
+
+static void RandomizeArray(int array[], int size);
+
+int main(void)
+{
+        int size, i;
+
+        scanf("%d", &size);
+
+        int array[size];
+        for (i = 0; i < size; i++) {
+                array[i] = i + 1;
+        }
+
+        RandomizeArray(array, size);
+
+        struct rbtree_s *treeRoot = InitRbtreeNil();
+        for (i = 0; i < size; i++) {
+                int temp = array[i];
+                printf("%d ", temp);
+                treeRoot = RbtreeInsert(treeRoot, temp);
+        }
+        printf("\n\n");
+
+        TreeWalkInorder(treeRoot);
+        printf("\n\n");
+
+        return 0;
+}
+
+static void RandomizeArray(int array[], int size)
+{
+        int i;
+        for (i = 1; i < size; i++) {
+                int tempIndex = rand() % i;
+                Swap(&array[i], &array[tempIndex]);
+        }
+}
