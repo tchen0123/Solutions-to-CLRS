81c4b7ef8c8a8bd899f4ad53c95582153436fda0
leohotfn <leohotfn@qq.com>
2016-04-26 21:10:52 +0800

    Fix bug

--- Chapter09-Medians-and-Order-Statistics/max_min.c
+++ Chapter09-Medians-and-Order-Statistics/max_min.c
@@ -22,6 +22,8 @@ int Maximum(int array[], int size)
                         array[i] = temp;
                 }
         }
+
+        return max;
 }
 
 /*
@@ -39,6 +41,8 @@ int Minimum(int array[], int size)
                         array[i] = temp;
                 }
         }
+
+        return min;
 }
 
 /*
@@ -48,20 +52,22 @@ void MaximumAndMinimum(int array[], int size, int *max, int *min)
 {
         int i, tempMax, tempMin;
 
+        tempMax = tempMin = array[0];
         for (i = 0; i < size - 1; i += 2) {
                 if (array[i] > array[i + 1]) {
-                        tempMax = array[i];
-                        tempMin = array[i + 1];
+                        if (array[i] > tempMax) {
+                                tempMax = array[i];
+                        }
+                        if (array[i + 1] < tempMin) {
+                                tempMin = array[i + 1];
+                        }
                 } else {
-                        tempMax = array[i + 1];
-                        tempMin = array[i];
-                }
-        }
-        for (; i < size; i++) {
-                if (array[i] > tempMax) {
-                        tempMax = array[i];
-                } else if (array[i] < tempMin) {
-                        tempMin = array[i];
+                        if (array[i + 1] > tempMax) {
+                                tempMax = array[i + 1];
+                        }
+                        if (array[i] < tempMin) {
+                                tempMin = array[i];
+                        }
                 }
         }
 
