93410c362d7c7887014647926714fbab5433f8fb
leohotfn <leohotfn@qq.com>
2016-04-26 22:35:56 +0800

    Add binary sum

---
+++ Chapter02-Getting-Started/binary_sum.h
@@ -0,0 +1,12 @@
+/*
+ * Solutions-to-CLRS/Chapter02-Getting-Started/binary_sum.h
+ *
+ * Copyright (c) 2016  Jiawei Feng
+ */
+
+#ifndef __BINARY_SUM_H_
+#define __BINARY_SUM_H_
+
+void BinarySum(int A[], int B[], int size, int sum[]);
+
+#endif
--- Chapter02-Getting-Started/test.c
+++ Chapter02-Getting-Started/test.c
@@ -5,6 +5,7 @@
  */
 
 #include "insertion_sort.h"
+#include "binary_sum.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -16,43 +17,93 @@ static inline void Swap(int *a, int *b)
 }
 
 static void RandomizeArray(int array[], int size);
+static void CreateBinary(int array[], int size);
 
 int main(void)
 {
-        int size, i;
+        int size, i, select;
+
+        printf("1.Insertion sort test\n");
+        printf("2.Binary numbers sum test\n");
+        printf("Please enter your choice:\n");
+        scanf("%d", &select);
 
         printf("How many integer numbers do you want to enter?\n");
         scanf("%d", &size);
 
         int array[size];
-        for (i = 0; i < size; i++) {
-                array[i] = i + 1;
+        int binaryA[size], binaryB[size], binarySum[11];
+
+        switch (select) {
+        case 1:
+                for (i = 0; i < size; i++) {
+                        array[i] = i + 1;
+                }
+
+                RandomizeArray(array, size);
+
+                printf("The input array:\n");
+                for (i = 0; i < size; i++) {
+                        printf("%d ", array[i]);
+                }
+                printf("\n\n");
+
+                InsertionSort(array, size);
+
+                printf("The array sorted:\n");
+                for (i = 0; i < size; i++) {
+                        printf("%d ", array[i]);
+                }
+                printf("\n\n");
+                break;
+        case 2:
+                /* Create binary number array */
+                CreateBinary(binaryA, size);
+                CreateBinary(binaryB, size);
+
+                /* Binary sum */
+                BinarySum(binaryA, binaryB, size, binarySum);
+
+                printf(" ");
+                for (i = 0; i < size; i++) {
+                        printf("%d", binaryA[i]);
+                }
+                printf("\n+\n");
+
+                printf(" ");
+                for (i = 0; i < size; i++) {
+                        printf("%d", binaryB[i]);
+                }
+                printf("\n=\n");
+
+                for (i = 0; i < size + 1; i++) {
+                        printf("%d", binarySum[i]);
+                }
+                printf("\n");
         }
-
-        RandomizeArray(array, size);
-
-        printf("The input array:\n");
-        for (i = 0; i < size; i++) {
-                printf("%d ", array[i]);
-        }
-        printf("\n\n");
-
-        InsertionSort(array, size);
-
-        printf("The array sorted:\n");
-        for (i = 0; i < size; i++) {
-                printf("%d ", array[i]);
-        }
-        printf("\n\n");
 
         return 0;
 }
 
+/*
+ * Randomize array.
+ */
 static void RandomizeArray(int array[], int size)
 {
         int i;
         for (i = 1; i < size; i++) {
                 int tempIndex = rand() % i;
                 Swap(&array[i], &array[tempIndex]);
+        }
+}
+
+/*
+ * Create binary number array.
+ */
+static void CreateBinary(int array[], int size)
+{
+        int i;
+        for (i = 0; i < size; i++) {
+                array[i] = rand() % 2;
         }
 }
