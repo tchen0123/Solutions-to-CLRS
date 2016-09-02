00a447f2dd9e3f0926b4f72faa7065d298d1462b
leohotfn <leohotfn@gmail.com>
2016-09-02 17:40:14 +0800

    Update merge_sort.cpp

--- Chapter02-Getting-Started/merge_sort.cpp
+++ Chapter02-Getting-Started/merge_sort.cpp
@@ -13,27 +13,13 @@ static inline void merge(std::vector<int> &array, std::size_t low, std::size_t h
                 rArray[j] = array[mid + 1 + j];
         }
 
-        for (std::size_t k = low, i = 0, j = 0;
+        for (std::size_t i = 0, j = 0, k = low;
              k <= high; ++k) {
-                if (lArray[i] <= rArray[j]) {
-                        array[k] = lArray[i];
-                        if (++i == lSize) {
-                                for (++k; k <= high; ++k) {
-                                        array[k] = rArray[j];
-                                        ++j;
-                                }
-                                break;
-                        }
+                if (j == rSize || (i < lSize && lArray[i] <= rArray[j])) {
+                        array[k] = lArray[i++];
                 }
                 else {
-                        array[k] = rArray[j];
-                        if (++j == rSize) {
-                                for (++k; k <= high; ++k) {
-                                        array[k] = lArray[i];
-                                        ++i;
-                                }
-                                break;
-                        }
+                        array[k] = rArray[j++];
                 }
         }
 }
